d483c4caed92a2d5d3136e53ee844b1419567754
findaiproperty <find.ai.property@gmail.com>
2025-11-14 11:50:35 +0100

    Fix import alias for pandas from 'pdf' to 'pd'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 import os
 from datetime import datetime
-import pandas as pdf
+import pandas as pd
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
